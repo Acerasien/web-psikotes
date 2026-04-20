a9601d829edb255f9d431780d221915fbfc3f592
Alexander <c14180178>
2026-04-20 10:45:58 +0700

    chore: remove temporary migration and test scripts

--- server/check_tests.py
+++
@@ -1,9 +0,0 @@
-from database import SessionLocal
-from models import Test
-
-db = SessionLocal()
-tests = db.query(Test).all()
-print("Tests in DB:")
-for t in tests:
-    print(f"- Name: '{t.name}', Code: '{t.code}'")
-db.close()
